5abe2f0d0851dbf46194d49a168bccb7080ba3be
vivekm500 <your-github-vivekkr91990@gmal.com>
2026-01-11 23:59:55 +0530

    first commit

---
+++ js_day_34_adv/script.js
@@ -0,0 +1,16 @@
+// PROMISES
+// ASYNC AWAWIT
+// setTimeout and setInterval
+
+
+// promises -> ek kaam jake karo
+// work is being done(in the priocess) -> pending state
+// work done(completed) -> resolved
+// work not done -> reject
+
+// facebook -> mera data lao
+// facebook p gya -> pending state
+// data aya -> resolve
+// data nhi aya -> reject
+
+new promise
