c86a8dad53b2d2e482f9de318ebd5f10f5e373a9
vivekm500 <vivekkr91990@gmail.com>
2026-01-19 00:23:44 +0530

    folder structure using vite bundler

--- reactjs_day_03/script.js
+++ reactjs_day_03/script.js
@@ -18,3 +18,5 @@ root.render(app())
 
 // VITE - ek bundler h jo folder bnake deta h
 // just go vs terminal and run 'npm create vite'
+// using command 'cd folderName' go to that folder and then run the command 'npm run dev' to run the server and get a localhost url
+// press 'h + enter' in the terminal to open help
