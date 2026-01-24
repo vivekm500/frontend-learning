bc3f3c5ea1c836e3eb2753bf547462e9a9f544b3
vivekm500 <vivekkr91990@gmail.com>
2026-01-24 22:00:07 +0530

    using tailwindcss in react

---
+++ reactjs_day_04_tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold bg-amber-800'>Suno, mere veero!</h1>
+
+      <h2 className='bg-red-400'>Hello guys</h2>
+    </div>
+  )
+}
+
+export default App
